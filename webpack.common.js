e41abe863301adc648c2ab5f9efb1f2c5a32fc36
Nazmus Sayad <247sayad@gmail.com>
2022-07-03 06:18:40 +0600

    Update webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,7 @@ const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 const PATH = {
-  mainJS: "src/index.js",
+  mainJS: "src/script.js",
   template: "src/index.html",
   output: "dist",
 }
