9810f07a4dbdaadff127c85347268b04c54a3029
Nazmus Sayad <247sayad@gmail.com>
2022-07-15 23:45:13 +0600

    Update webpack.dev.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,9 +3,14 @@ const { CONFIG, PATH } = require("./webpack.common")
 CONFIG.mode = "development"
 CONFIG.devtool = "source-map"
 CONFIG.devServer = {
+  client: {
+    logging: "none",
+  },
+  
   static: {
     directory: PATH.output,
   },
+  
   port: 80,
   open: true,
   hot: true,
