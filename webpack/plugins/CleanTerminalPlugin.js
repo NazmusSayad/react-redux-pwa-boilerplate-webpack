0a25dc92ab089d92768b6b780a2761fd84294f4d
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2022-09-18 20:21:19 +0600

    Update CleanTerminalPlugin.js

--- webpack/plugins/CleanTerminalPlugin.js
+++ webpack/plugins/CleanTerminalPlugin.js
@@ -1,16 +1,8 @@
 class CleanTerminalPlugin {
   apply(compiler) {
-    let hook = compiler.hooks.beforeCompile
-
-    hook.tap('CleanTerminalPlugin', () => {
-      this.clearConsole()
+    compiler.hooks.beforeCompile.tap('CleanTerminalPlugin', () => {
+      console.clear()
     })
-  }
-
-  clearConsole() {
-    process.stdout.write(
-      process.platform === 'win32' ? '\x1B[2J\x1B[0f' : '\x1B[2J\x1B[3J\x1B[H'
-    )
   }
 }
 
